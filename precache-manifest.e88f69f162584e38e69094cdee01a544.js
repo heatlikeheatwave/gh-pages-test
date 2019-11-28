7ea15d577a028c8a47b48a38ed22be98cbb94bd2
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2019-11-28 10:51:27 -0500

    Updates

---
+++ precache-manifest.e88f69f162584e38e69094cdee01a544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b259c343b37ddfad021822793415d6",
+    "url": "/gh-pages-test/index.html"
+  },
+  {
+    "revision": "8d4de4dcec5fc2b25e75",
+    "url": "/gh-pages-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1571a2e316199e160c2a",
+    "url": "/gh-pages-test/static/js/2.889fc0e7.chunk.js"
+  },
+  {
+    "revision": "8d4de4dcec5fc2b25e75",
+    "url": "/gh-pages-test/static/js/main.f143f3d7.chunk.js"
+  },
+  {
+    "revision": "cc77b7c2c23704c71c49",
+    "url": "/gh-pages-test/static/js/runtime-main.f2d85256.js"
+  }
+]);
